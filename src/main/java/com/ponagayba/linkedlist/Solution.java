59f56046426259b328cc58395ed0babc68b545f7
ponaguynik <ponaguynik@gmail.com>
2017-07-13 22:06:38 +0300

    Refactor Solution. Solution.addTwoNumbers() via recursion

--- src/main/java/com/ponagayba/linkedlist/Solution.java
+++ src/main/java/com/ponagayba/linkedlist/Solution.java
@@ -1,14 +1,14 @@
 package com.ponagayba.linkedlist;
 
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Solution {
 
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(0);
-        ListNode l2 = new ListNode(1);
-        fillUpListRandomly(l1);
-        fillUpListRandomly(l2);
+        ListNode l1 = new ListNode(5);
+        ListNode l2 = new ListNode(5);
+        l1.next = new ListNode(9);
+        l1.next.next = new ListNode(9);
+        l1.next.next.next = new ListNode(9);
         System.out.println("Linked list 1:");
         printList(l1);
         System.out.println("Linked list 2:");
@@ -19,54 +19,25 @@ public class Solution {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode result = new ListNode(l1.value + l2.value);
-        ListNode l1Current = l1, l2Current = l2, resultCurrent = result;
-
-        int rest = 0;
-        while (l1Current.next != null && l2Current.next != null) {
-            int sum = l1Current.next.value + l2Current.next.value + rest;
-            if (sum < 10) {
-                resultCurrent.next = new ListNode(sum);
-                rest = 0;
-            } else {
-                sum = sum - 10;
-                resultCurrent.next = new ListNode(sum);
-                rest = 1;
-            }
-            resultCurrent = resultCurrent.next;
-            l1Current = l1Current.next;
-            l2Current = l2Current.next;
-        }
-
-        while (l1Current.next != null) {
-            resultCurrent.next = new ListNode(l1Current.next.value + rest);
-            if (rest == 1)
-                rest = 0;
-            resultCurrent = resultCurrent.next;
-            l1Current = l1Current.next;
-        }
-        while (l2Current.next != null) {
-            resultCurrent.next = new ListNode(l2Current.next.value + rest);
-            if (rest == 1)
-                rest = 0;
-            resultCurrent = resultCurrent.next;
-            l2Current = l2Current.next;
-        }
-
-        if (rest == 1)
-            resultCurrent.next = new ListNode(rest);
-
-        return result;
+        return addTwoNumbers(l1, l2, 0);
     }
 
-    public static void fillUpListRandomly(ListNode root) {
-        ListNode current = root;
-        int size = ThreadLocalRandom.current().nextInt(1, 5);
-        for (int i = 0; i < size; i++) {
-            current.next = new ListNode(ThreadLocalRandom.current().nextInt(0, 10));
-
-            current = current.next;
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2, int add) {
+        int sum = l1.value + l2.value + add;
+        add = 0;
+        if (sum > 9) {
+            sum -= 10;
+            add = 1;
         }
+        ListNode result = new ListNode(sum);
+        if (l1.next == null && l2.next == null && add == 0)
+            return result;
+        if (l1.next == null)
+            l1.next = new ListNode(0);
+        if (l2.next == null)
+            l2.next = new ListNode(0);
+        result.next = addTwoNumbers(l1.next, l2.next, add);
+        return result;
     }
 
     public static void printList(ListNode root) {
